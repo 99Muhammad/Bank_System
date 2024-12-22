8677c3746d1aca2334f59fffa5475b4bbde29483
Muhammad Ibraheem <MuhammadIbraheem99m@gmail.com>
2024-12-22 07:58:59 +0300

    tracking

--- BankSystemProject/Repositories/Service/TrackingLoggedInUsersService.cs
+++ BankSystemProject/Repositories/Service/TrackingLoggedInUsersService.cs
@@ -30,6 +30,81 @@ namespace BankSystemProject.Repositories.Service
 
             return loggedInUsers;
         }
+
+        public async Task<List<Res_LoggedInUsersDto>> GetLoggedInUsersByUserNameAsync(string userName)
+        {
+            var loggedInUsers = await _dbContext.TrackingLoggedInUsers
+                .Include(tlu => tlu.users)
+                .Where(tlu => tlu.users.UserName.Contains(userName))
+                .OrderByDescending(tlu => tlu.LoginTime)
+                .Select(tlu => new Res_LoggedInUsersDto
+                {
+                    Username = tlu.users.UserName,
+                    Email = tlu.users.Email,
+                    Role = tlu.users.Role,
+                    LoginTime = tlu.LoginTime
+                })
+                .ToListAsync();
+
+            return loggedInUsers;
+        }
+
+        public async Task<List<Res_LoggedInUsersDto>> GetLoggedInUsersByDateAsync(DateTime loginDate)
+        {
+            var loggedInUsers = await _dbContext.TrackingLoggedInUsers
+                .Include(tlu => tlu.users)
+                .Where(tlu => tlu.LoginTime.Date == loginDate.Date)
+                .OrderByDescending(tlu => tlu.LoginTime)
+                .Select(tlu => new Res_LoggedInUsersDto
+                {
+                    Username = tlu.users.UserName,
+                    Email = tlu.users.Email,
+                    Role = tlu.users.Role,
+                    LoginTime = tlu.LoginTime
+                })
+                .ToListAsync();
+
+            return loggedInUsers;
+        }
+
+        public async Task<List<Res_LoggedInUsersDto>> GetLoggedInUsersByRoleAsync(string role)
+        {
+            var loggedInUsers = await _dbContext.TrackingLoggedInUsers
+                .Include(tlu => tlu.users)
+                .Where(tlu => tlu.users.Role.Contains(role))
+                .OrderByDescending(tlu => tlu.LoginTime)
+                .Select(tlu => new Res_LoggedInUsersDto
+                {
+                    Username = tlu.users.UserName,
+                    Email = tlu.users.Email,
+                    Role = tlu.users.Role,
+                    LoginTime = tlu.LoginTime
+                })
+                .ToListAsync();
+
+            return loggedInUsers;
+        }
+
+        public async Task<List<Res_LoggedInUsersDto>> GetLoggedInUsersLastMonthAsync()
+        {
+            var firstDayOfLastMonth = new DateTime(DateTime.Now.AddMonths(-1).Year, DateTime.Now.AddMonths(-1).Month, 1);
+            var lastDayOfLastMonth = firstDayOfLastMonth.AddMonths(1).AddDays(-1);
+
+            var loggedInUsers = await _dbContext.TrackingLoggedInUsers
+                .Include(tlu => tlu.users)
+                .Where(tlu => tlu.LoginTime >= firstDayOfLastMonth && tlu.LoginTime <= lastDayOfLastMonth)
+                .OrderByDescending(tlu => tlu.LoginTime)
+                .Select(tlu => new Res_LoggedInUsersDto
+                {
+                    Username = tlu.users.UserName,
+                    Email = tlu.users.Email,
+                    Role = tlu.users.Role,
+                    LoginTime = tlu.LoginTime
+                })
+                .ToListAsync();
+
+            return loggedInUsers;
+        }
     }
     
     
